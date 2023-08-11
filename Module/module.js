6e93685b49abb12af4b84941cf9d79250d72fea3
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-08-11 15:11:40 +0100

    Different syntax for exporting using modules

--- Module/module.js
+++ Module/module.js
@@ -1,5 +1,7 @@
 const names = require('./names')
 const sayHi = require('./utils')
+const data = require('./different_syntax')
+console.log(data)
 sayHi('susan')
 sayHi(names.john)
 sayHi(names.peter)
