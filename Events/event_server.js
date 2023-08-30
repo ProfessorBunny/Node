72a8088b54d6f61c27ec79dd7954d28d34e985d0
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-08-30 14:31:05 +0100

    file updated

--- Events/event_server.js
+++ Events/event_server.js
@@ -1,8 +1,6 @@
 const http = require('http')
 
-// const server = http.createServer((req, res) => {
-//   res.end('Welcome')
-// })
+
 
 // Using Event Emitter API
 const server = http.createServer()
