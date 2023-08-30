537b99f092b33c859071fd42f41941cbe75cdf86
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-08-30 14:31:35 +0100

    imports updated

--- Async/async-await.js
+++ Async/async-await.js
@@ -1,7 +1,5 @@
 const { readFile, writeFile } = require('fs').promises
-// const util = require('util')
-// const readFilePromise = util.promisify(readFile)
-// const writeFilePromise = util.promisify(writeFile)
+
 
 const start = async () => {
     try {
