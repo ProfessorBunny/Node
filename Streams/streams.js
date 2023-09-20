57b429dcb34dc1dc2252e843b6e5edf35f66cc51
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-09-20 23:23:17 +0100

    streams

--- Streams/streams.js
+++ Streams/streams.js
@@ -1,7 +1,4 @@
 const { createReadStream } = require('fs')
-
-// default 64kb
-// last buffer - remainder
 // highWaterMark - control size
 // const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
 // const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
