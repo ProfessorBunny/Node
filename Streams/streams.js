99f315914d797d47af345110ff49a5e87e987bc5
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-09-23 14:48:23 +0100

    Adding additional parameters with stream

--- Streams/streams.js
+++ Streams/streams.js
@@ -1,10 +1,9 @@
 const { createReadStream } = require('fs')
 // highWaterMark - control size
 // const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
-// const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
-const stream = createReadStream('./content/big.txt')
+const stream = createReadStream('../content/big.txt', { highWaterMark: 90000, encoding: 'utf8' })
+// const stream = createReadStream('./content/big.txt')
 
 stream.on('data', (result) => {
     console.log(result)
 })
-// stream.on('error', (err) => console.log(err))
