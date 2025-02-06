87311f5be7ad1305204c379bb8b73d6bea10c558
ProfessorBuny <nikunjraghav24@gmail.com>
2025-02-06 18:48:30 +0000

    creating a big txt file

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-const { readFile } = require("fs").promises;
-
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
-  });
-};
-getText("./content/first.txt")
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
+const { writeFileSync } = require("fs");
+for (let i = 0; i < 10000; i++) {
+  writeFileSync("./content/big.txt", `hello world ${i}\n`, { flag: "a" });
+}
