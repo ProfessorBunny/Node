e1a65ce981417d161fba9a00116f37f595425af0
ProfessorBuny <nikunjraghav24@gmail.com>
2025-02-06 02:44:54 +0000

    working with os module

--- Module/os-module.js
+++ Module/os-module.js
@@ -1,16 +1,16 @@
-const os = require('os')
+const os = require("os");
 
 // info about current user
-const user = os.userInfo()
-console.log(user)
+const user_info = os.userInfo();
+console.log(user_info);
 
-// method returns the system uptime in seconds
-console.log(`The System Uptime is ${os.uptime()} seconds`)
+//this method returns the system uptime in seconds
+console.log(`The System Uptime is ${os.uptime()} seconds`);
 
 const currentOS = {
-    name: os.type(),
-    release: os.release(),
-    totalMem: os.totalmem(),
-    freeMem: os.freemem(),
-}
-console.log(currentOS)                                                                              
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+};
+console.log(currentOS);
